0440aa55c1a10f43d4e4b37ab2da6a9e873493f5
Jenna Zeigen <jenna.zeigen@gmail.com>
2012-10-12 12:45:59 -0400

    db-front-end interface

--- expensieve.js
+++ expensieve.js
@@ -1,6 +1,7 @@
 var mongo = require('mongodb'),
   Server = mongo.Server,
-  Db = mongo.Db;
+  Db = mongo.Db,
+  ObjectID = mongo.ObjectID;
 
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 var db = new Db('test', server);
@@ -22,20 +23,44 @@ var app = express();
 app.listen(1337);
 
 app.use("/static", express.static(__dirname+'/static'));
+app.use(express.bodyParser());
 
+//request to the home page
 app.get('/', function(req, res){
 	res.sendfile('index.html');
 });
 
+//request to READ a specific receipt
 app.get('/receipts/:id', function(req, res){
-	db.collection('expensieve').find({'_id': req.params.id}).toArray(function(err, docs){
+	db.collection('expensieve').find({'_id': new ObjectID(req.params.id)}).toArray(function(err, docs){
 		res.json(docs)
 	});
 });
 
-app.post('/receipts', function(req, res){
-	db.collection('expensieve').save().toArray(function(err, docs){
+//request to READ the entire receipts collection
+app.get('/receipts', function(req, res){
+	db.collection('expensieve').find().toArray(function(err, docs){
 		res.json(docs)
 	});
+});
+
+//request to CREATE a new entry
+app.post('/receipts', function(req, res){
+	db.collection('expensieve').save(req.body);
+	res.send(req.body);
+});
+
+//request to UPDATE an existing entry
+app.put('/receipts/:id', function(req, res){
+	req.body.amount = parseFloat(req.body.amount)
+	req.body._id = new ObjectID(req.body._id)
+	db.collection('expensieve').update({'_id': new ObjectID(req.params.id)}, req.body);
+	res.send(req.body);
+});
+
+//request to DELETE an existing entry
+app.delete('/receipts/:id', function(req, res){
+	db.collection('expensieve').remove({_id: new ObjectID(req.params.id)});
+	res.send(req.body)
 });
 
--- static/splinter.js
+++ static/splinter.js
@@ -13,17 +13,19 @@ window.ReceiptList = Backbone.Collection.extend({
 	url: '/receipts'
 });
 
+window.receipts = new ReceiptList;
+
 window.ReceiptView = Backbone.View.extend({
 	tagName: 'tr',
 	className: 'expenseLine',
 	model: Receipt,
 
-	template: _.template("<p class='name'></p> <input class='name-edit' type='text' /> <p class='amount'></p><input class='amount-edit' type='number' /> <p class='shared'></p><input class='shared-edit' type='text' />"),
+	template: _.template("<td class='name-box'><p class='name'></p> <input class='name-edit' type='text' /></td>   <td class='amt-box'><p class='amount'></p><input class='amount-edit' type='number' /></td>   <td class='shared-box'><p class='shared'></p><input class='shared-edit' type='text' /></td>   <td><span class='delete'> x </span></td>"),
 
 	events: {
       	"click .name": "editWho",
       	"click .amount": "editAmt",
-      	"click .memo": "editShared",
+      	"click .shared": "editShared",
 
       	"click .delete": "clear",
 
@@ -37,7 +39,7 @@ window.ReceiptView = Backbone.View.extend({
 	},
 
 	initialize: function() {
-      _.bindAll(this, 'render', 'exit');
+      _.bindAll(this, 'render', 'exitName', 'exitAmount', 'exitShared');
       this.model.bind('change', this.render);
       this.model.view = this;
       this.category = this.model.get('category');
@@ -48,11 +50,14 @@ window.ReceiptView = Backbone.View.extend({
 		var amount = this.model.get('amount');
 		var shared = this.model.get('shared');
 
-		this.$('.name').text(text);
-		this.$('.name-edit').val(text)
+		this.$('.name').text(name);
+		this.$('.name-edit').val(name)
 
-		this.$('.amount').text(text);
-		this.$('.amount-edit').val(text)
+		this.$('.amount').text(amount);
+		this.$('.amount-edit').val(amount)
+
+		this.$('.shared').text(shared);
+		this.$('.shared-edit').val(shared)
 
 		this.nameInput = this.$('.name-edit');
 		this.amtInput = this.$('.amount-edit');
@@ -67,33 +72,32 @@ window.ReceiptView = Backbone.View.extend({
 
 	
 	editWho: function() {
-		$(this.el).children('.name').addClass("editing");
+		$(this.el).children('.name-box').addClass("editing");
 		this.nameInput.focus();
 	},
 
 	editAmt: function() {
-		$(this.el).children('.amount').addClass("editing");
+		$(this.el).children('.amt-box').addClass("editing");
 		this.amtInput.focus();
 	},
 
 	editShared: function() {
-		$(this.el).children('.shared').addClass("editing");
+		$(this.el).children('.shared-box').addClass("editing");
 		this.shareInput.focus();
 	},
-
 	
 	exitName: function() {
-		$(this.el).children('.name').removeClass("editing");
+		$(this.el).children('.name-box').removeClass("editing");
 		this.model.save({'name': this.nameInput.val()});
 	},
 
 	exitAmount: function() {
-		$(this.el).children('.amount').removeClass("editing");
+		$(this.el).children('.amount-box').removeClass("editing");
 		this.model.save({'amount': this.amtInput.val()});
 	},
 
 	exitShared: function() {
-		$(this.el).children('.shared').removeClass("editing");
+		$(this.el).children('.shared-box').removeClass("editing");
 		this.model.save({'shared': this.shareInput.val()});
 	},
 
@@ -120,3 +124,80 @@ window.ReceiptView = Backbone.View.extend({
     	this.model.remove();
     }
 })
+
+window.AppView = Backbone.View.extend({
+	el: $('#wrapper'),
+
+	// events: {
+ //      "click #send" : "addToDB",
+ //      "click #add-button" : "showAdd"
+ //    },
+
+    initialize: function() {
+    	_.bindAll(this, 'addOne', 'addAll', 'render');
+    
+      	this.news = this.$("#add");
+      
+      	receipts.bind('add', this.addOne);
+      	receipts.bind('refresh', this.addAll);
+      	receipts.bind('all', this.render);
+    
+      	receipts.fetch(
+      		{success: this.addAll}
+      	);
+    },
+
+    addOne: function(item) {
+      	var element = new ReceiptView({model:item}).render().el;
+      	$("#expense-list").append(element);
+    },
+    
+    addAll: function() {
+      receipts.each(this.addOne);
+    },
+
+});
+
+window.App = new AppView;
+
+$(document).ready(function(){
+
+
+$('#send').click(function(){
+    	console.log('send')
+	  	receipts.create({
+	    	'name': $('#new-name').val(),
+	    	'amount': $('#new-amount').val(),
+	    	'shared': $('#new-share').val(),
+	    	'shares': ['ummmm']
+	  	});
+	    $('#add').children('input').val('');
+	    $('#add-button').removeClass('adding');
+	    $('#add').removeClass('adding');
+	});
+
+	$('#add-button').click(function(){
+		$('#add').addClass('adding');
+		$('#add-button').addClass('adding');
+
+		$('#splitEqual').click(function(){
+			console.log('equal')
+			var numSplit = $('#new-share').val().split(',').length+1;
+			var amount = $('new-amount').val();
+			var splits = [];
+			var total;
+			for (i=0; i<numSplit; i++){
+				split = Math.round(amount/numSplit*Math.pow(10,2))/Math.pow(10,2);
+				splits.push(split)
+				total+=split
+			}
+			console.log(splits)
+			//var index = 0
+			// while(total!==amount){
+			// 	splits[index]+=0.01
+			// 	total+=0.01
+			// 	console.log('going')
+			// }
+		});
+	});
+});
